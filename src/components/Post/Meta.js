52a5197b8f96456a9651723249feabcf046a00d0
Samreen <samk19.blog@gmail.com>
2018-09-07 19:52:34 +0530

    Remove author related stuff

--- src/components/Post/Meta.js
+++ src/components/Post/Meta.js
@@ -3,19 +3,15 @@ import PropTypes from "prop-types";
 import Link from "gatsby-link";
 
 import FaCalendar from "react-icons/lib/fa/calendar";
-import FaUser from "react-icons/lib/fa/user";
 import FaTag from "react-icons/lib/fa/tag";
 
 const Meta = props => {
-  const { prefix, author: authorName, category, theme } = props;
+  const { prefix, category, theme } = props;
 
   return (
     <p className="meta">
       <span>
         <FaCalendar size={18} /> {prefix}
-      </span>
-      <span>
-        <FaUser size={18} /> {authorName}
       </span>
       {category && (
         <span>
@@ -56,7 +52,6 @@ const Meta = props => {
 
 Meta.propTypes = {
   prefix: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
   category: PropTypes.string,
   theme: PropTypes.object.isRequired
 };
